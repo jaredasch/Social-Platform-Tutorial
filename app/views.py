f69d567762f808548390b87b2e83af6ef63da0f6
jaredasch1 <jaredasch1@gmail.com>
2017-09-02 01:14:58 -0400

    Add pagination to all pages w/ posts

--- app/views.py
+++ app/views.py
@@ -4,8 +4,8 @@ from flask_login import login_user, login_required, logout_user, current_user, l
 from .forms import SignupForm, LoginForm, PostForm, UpdatePasswordForm, ForgotForm, UpdateUsernameForm
 from config import ADMINS, ALLOWED_EXTENSIONS, UPLOAD_FOLDER, POSTS_PER_PAGE
 from .emails import send_email
-from werkzeug import secure_filename
 import datetime, csv, os
+from werkzeug import secure_filename
 
 lm.login_view = "login"
 
@@ -22,17 +22,19 @@ def load_user(id):
 
 @app.route('/', methods=['GET'])
 def index():
+    page = int(request.args.get('page')) if 'page' in request.args else 1
     if current_user.is_authenticated:
-        posts = current_user.followed_posts()
+        posts = current_user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
     else:
-        posts = models.Post.query.order_by("date desc").all()
+        posts = models.Post.query.order_by("date desc").paginate(page, POSTS_PER_PAGE, False)
     form = PostForm()
     return render_template("user/index.html", title='Home', posts=posts, form=form)
 
 
-@app.route('/posts/all', methods=["GET"])
+@app.route('/posts', methods=["GET"])
 def all_posts():
-    posts = models.Post.query.order_by("date desc").all()
+    page = int(request.args.get('page')) if 'page' in request.args else 1
+    posts = models.Post.query.order_by("date desc").paginate(page, POSTS_PER_PAGE, False)
     form = PostForm()
     return render_template("user/index.html", title='Home', posts=posts, form=form)
 
@@ -51,7 +53,7 @@ def signup():
             new_user = models.User(name=form.fname.data + ' ' + form.lname.data, fname=form.fname.data, lname=form.lname.data, email=form.email.data, nickname=form.nickname.data, username=form.username.data)
             new_user.set_password(form.password.data)
             if form.email.data in ADMINS:
-               new_user.is_admin = True
+                new_user.is_admin = True
             else:
                 new_user.is_admin = False
             db.session.add(new_user)
@@ -85,25 +87,24 @@ def login():
     return render_template("general/login.html", title="Log In", form=form)
 
 
-@app.route('/profile/<user>')
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(request.args.get("next") or url_for("index"))
+
+
+@app.route('/profile/<user>', methods=['GET'])
 def profile(user):
+    page = int(request.args.get('page')) if 'page' in request.args else 1
     if models.User.query.filter_by(username=user).count() == 0:
         return render_template('user/user_not_found.html')
     edit_form = PostForm()
     change_password_form = UpdatePasswordForm()
     user = models.User.query.filter_by(username=user).one()
-    posts = models.Post.query.filter_by(author=user).order_by("date desc")
+    posts = models.Post.query.filter_by(author=user).order_by("date desc").paginate(page, POSTS_PER_PAGE, False)
     return render_template("user/profile.html", user=user, posts=posts, title="%s %s" % (user.fname, user.lname), edit_form=edit_form, password_form=change_password_form)
 
-@app.route('/posts/<user>/<int:page>')
-@login_required
-def posts(user, page = 1):
-    if models.User.query.filter_by(username=user).count() == 0:
-        return render_template('user/user_not_found.html')
-    edit_form = PostForm()
-    user = models.User.query.filter_by(username=user).one()
-    posts = user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
-    return render_template("user/posts.html", user=user, posts=posts, title="%s %s" % (user.fname, user.lname), edit_form=edit_form)
 
 @app.route('/post', methods=['POST'])
 @login_required
@@ -116,23 +117,6 @@ def post():
         return redirect(url_for("index"))
     print form.body.errors
     return redirect(url_for("index"))
-
-
-@app.route('/logout', methods=['GET'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(request.args.get("next") or url_for("index"))
-
-
-@app.route('/delete_post/<int:id>', methods=['GET'])
-@login_required
-def delete_post(id):
-    post = models.Post.query.filter_by(id=id).one()
-    if post.author == current_user or current_user.is_admin:
-        db.session.delete(post)
-        db.session.commit()
-    return redirect(request.referrer)
 
 
 @app.route('/follow/<username>', methods=["GET"])
@@ -152,6 +136,43 @@ def unfollow(username):
     user = models.User.query.filter_by(username=current_user.username).one().unfollow(user_to_follow)
     db.session.add(user)
     db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/edit_post/<int:id>', methods=["POST"])
+@login_required
+def edit_post(id):
+    form = PostForm()
+    post = models.Post.query.filter_by(id=id).one()
+    if post.author == current_user or current_user.is_admin:
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/delete_post/<int:id>', methods=['GET'])
+@login_required
+def delete_post(id):
+    post = models.Post.query.filter_by(id=id).one()
+    if post.author == current_user or current_user.is_admin:
+        db.session.delete(post)
+        db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/update_password/', methods=['POST'])
+def update_password():
+    form = UpdatePasswordForm()
+    if form.validate_on_submit():
+        current_user.set_password(form.new_password.data)
+        db.session.add(current_user)
+        db.session.commit()
+        return redirect(request.referrer)
+    errors = []
+    for field in form:
+        errors += field.errors
+    flash(errors)
     return redirect(request.referrer)
 
 
@@ -175,64 +196,6 @@ def user_search():
     return render_template('user/users.html', title="Results for %s" % tag, users=results)
 
 
-@app.route('/admin/')
-@login_required
-def admin_controls():
-    form = PostForm()
-    if not current_user.is_admin:
-        return redirect(url_for("index"))
-    else:
-        posts = models.Post.query.order_by("date desc").all()
-        return render_template('admin/admin_panel.html', title="Admin Panel", form=form, postCount=models.Post.query.count(), userCount=models.User.query.count(), posts=posts)
-
-
-@app.route('/edit_post/<int:id>', methods=["POST"])
-@login_required
-def edit_post(id):
-    form = PostForm()
-    post = models.Post.query.filter_by(id=id).one()
-    if post.author == current_user or current_user.is_admin:
-        post.body = form.body.data
-        db.session.add(post)
-        db.session.commit()
-    return redirect(request.referrer)
-
-
-@app.route('/update_password/', methods=['POST'])
-def update_password():
-    form = UpdatePasswordForm()
-    if form.validate_on_submit():
-        current_user.set_password(form.new_password.data)
-        db.session.add(current_user)
-        db.session.commit()
-        return redirect(request.referrer)
-    errors = []
-    for field in form:
-        errors += field.errors
-    flash(errors)
-    return redirect(request.referrer)
-
-
-@app.route('/users/csv', methods=['GET'])
-@login_required
-def get_users_csv():
-    if current_user.is_admin:
-        users = [models.User.query.first().__dict__.keys()]
-        for u in models.User.query.all():
-            user_temp = []
-            for key, value in u.__dict__.items():
-                user_temp += [value]
-            users += [user_temp]
-        with open("app/data/users.csv", "wb") as f:
-            writer = csv.writer(f)
-            writer.writerows(users)
-        return send_file('data/users.csv',
-                         mimetype='text/csv',
-                         attachment_filename='data/users.csv',
-                         as_attachment=True)
-    return redirect(url_for("index"))
-
-
 @app.route('/delete_user/<int:id>', methods=["GET"])
 @login_required
 def delete_user(id):
@@ -248,31 +211,6 @@ def delete_user(id):
     return redirect(url_for("index"))
 
 
-@app.route('/add_admin/<int:id>', methods=["GET"])
-@login_required
-def add_admin(id):
-    if current_user.is_admin:
-        u = models.User.query.filter_by(id=id).one()
-        u.is_admin = True
-        db.session.add(u)
-        db.session.commit()
-    return redirect(request.referrer)
-
-
-@app.route('/remove_admin/<int:id>', methods=["GET"])
-@login_required
-def remove_admin(id):
-    if current_user.is_admin:
-        u = models.User.query.filter_by(id=id).one()
-        if u.email not in ADMINS:
-            u.is_admin = False
-            db.session.add(u)
-            db.session.commit()
-        else:
-            flash("You Can't Remove this Admin", "admin-error")
-    return redirect(request.referrer)
-
-
 @app.route('/forgot_password', methods=["GET", "POST"])
 def forgot_password():
     form = ForgotForm()
@@ -281,7 +219,7 @@ def forgot_password():
             user = models.User.query.filter_by(email=form.email.data).one()
         else:
             return render_template("general/forgot_password.html", title="Forgot Password", form=form)
-        user_request = models.ResetRequest(user=user, date = datetime.datetime.utcnow())
+        user_request = models.ResetRequest(user=user, date=datetime.datetime.utcnow())
         db.session.add(user_request)
         requests_to_delete = db.session.query(models.ResetRequest).filter(models.ResetRequest.date >= datetime.datetime.utcnow() + datetime.timedelta(minutes=10)).all()
         for req in requests_to_delete:
@@ -314,7 +252,7 @@ def forgot_username():
             user = models.User.query.filter_by(email=form.email.data).one()
         else:
             return render_template("general/forgot_username.html", title="Forgot Username", form=form)
-        user_request = models.ResetRequest(user=user, date = datetime.datetime.utcnow())
+        user_request = models.ResetRequest(user=user, date=datetime.datetime.utcnow())
         db.session.add(user_request)
         requests_to_delete = db.session.query(models.ResetRequest).filter(models.ResetRequest.date >= datetime.datetime.utcnow() + datetime.timedelta(minutes=10)).all()
         for req in requests_to_delete:
@@ -366,3 +304,60 @@ def upload_profile_pic():
 def get_profile_pic(user_id):
     filename = models.User.query.filter_by(id=user_id).one().profile_image
     return send_from_directory('data/uploads/', filename)
+
+
+@app.route('/add_admin/<int:id>', methods=["GET"])
+@login_required
+def add_admin(id):
+    if current_user.is_admin:
+        u = models.User.query.filter_by(id=id).one()
+        u.is_admin = True
+        db.session.add(u)
+        db.session.commit()
+    return redirect(request.referrer)
+
+
+@app.route('/remove_admin/<int:id>', methods=["GET"])
+@login_required
+def remove_admin(id):
+    if current_user.is_admin:
+        u = models.User.query.filter_by(id=id).one()
+        if u.email not in ADMINS:
+            u.is_admin = False
+            db.session.add(u)
+            db.session.commit()
+        else:
+            flash("You Can't Remove this Admin", "admin-error")
+    return redirect(request.referrer)
+
+
+@app.route('/admin')
+@login_required
+def admin_controls():
+    page = int(request.args.get('page')) if 'page' in request.args else 1
+    form = PostForm()
+    if not current_user.is_admin:
+        return redirect(url_for("index"))
+    else:
+        posts = models.Post.query.order_by("date desc").paginate(page, POSTS_PER_PAGE, False)
+        return render_template('admin/admin_panel.html', title="Admin Panel", edit_form=form, postCount=models.Post.query.count(), userCount=models.User.query.count(), posts=posts)
+
+
+@app.route('/users/csv', methods=['GET'])
+@login_required
+def get_users_csv():
+    if current_user.is_admin:
+        users = [models.User.query.first().__dict__.keys()]
+        for u in models.User.query.all():
+            user_temp = []
+            for key, value in u.__dict__.items():
+                user_temp += [value]
+            users += [user_temp]
+        with open("app/data/users.csv", "wb") as f:
+            writer = csv.writer(f)
+            writer.writerows(users)
+        return send_file('data/users.csv',
+                         mimetype='text/csv',
+                         attachment_filename='data/users.csv',
+                         as_attachment=True)
+    return redirect(url_for("index"))
